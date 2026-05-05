dde3dd475fca9d83ee1f67755ec94e750559b5c0
georgehadjisavva <georgex8@gmail.com>
2026-05-05 09:21:04 +0300

    introducing backoff strategy

---
+++ backoff.go
@@ -0,0 +1,17 @@
+package retry
+
+import "time"
+
+type BackoffStrategy func(attempt int ,initialDelay time.Duration) time.Duration
+
+var Fixed BackoffStrategy = func(attempt int, d time.Duration) time.Duration {
+	return d
+}
+
+var Linear BackoffStrategy = func(attempt int, d time.Duration) time.Duration {
+	return d * time.Duration(attempt+1)
+}
+
+var Exponential BackoffStrategy = func(attempt int, d time.Duration) time.Duration {                                                                                                                              
+      return d * (1 << attempt)  // d * 2^attempt
+}  
--- options.go
+++ options.go
@@ -5,8 +5,12 @@ import "time"
 type config struct {
 	maxAttempts int           // how many times to try total
 	fixedDelay  time.Duration //how long to wait between attempts
+	backoff BackoffStrategy
+	initialDelay time.Duration // base delay fed into strategy
+	maxDelay time.Duration // cap - no delay ever exceed this duration
 }
 
+//we this we give the ability to consumers to customize behaviour without exposing conifg
 type Option func(*config)
 
 func defaultConfig() *config {
@@ -22,3 +26,7 @@ func WithMaxAttempts(n int) Option {
 func WithFixedDelay(d time.Duration) Option {
 	return func(c *config) { c.fixedDelay = d }
 }
+
+func WithBackoff(backoff BackoffStrategy) Option {
+	return func(c *config) {c.backoff = backoff}
+} 
